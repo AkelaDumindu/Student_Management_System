99aa35b935d17363f07856236116fbe5b757024f
AkelaDumindu <akeladumindu2000@gmail.com>
2024-11-01 01:12:32 +0530

    created student  section

---
+++ src/lk/akeladumindu/lms/entity/Book.java
@@ -0,0 +1,35 @@
+package lk.akeladumindu.lms.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Book {
+
+    private long id;
+
+    private String title;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Book() {
+    }
+
+    public Book(long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+}
